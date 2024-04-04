b1e996c428c011aec4dacbe30520fafbddec39eb
achalbajpai <111895109+achalbajpai@users.noreply.github.com>
2024-04-04 18:44:29 +0530

    Solved 1521
    Using nested loops and incrementing

---
+++ leetcode/java/sol1521.java
@@ -0,0 +1,18 @@
+class sol1521 {
+    public int numIdenticalPairs(int[] nums) {
+        int n = nums.length;
+        int count = 0;
+
+        for (int i = 0; i < n - 1; i++) {
+            int num = nums[i];
+
+            for (int j = i + 1; j < n; j++) {
+                if (nums[j] == num) {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+}
