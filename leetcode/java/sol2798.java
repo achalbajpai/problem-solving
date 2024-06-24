fa39a9237cf0b9a808e452f13d27a37163535190
Achal Bajpai <111895109+achalbajpai@users.noreply.github.com>
2024-06-24 09:32:46 +0530

    Update sol2798.java

--- leetcode/java/sol2798.java
+++ leetcode/java/sol2798.java
@@ -1,9 +1,10 @@
 class Solution {
     public int numberOfEmployeesWhoMetTarget(int[] hours, int target) {
         int count = 0;
-        for(int i = 0; i < hours.length; i++)
+     //    for(int i = 0; i < hours.length; i++)
+        for (int hour : hours) // for each
         {
-            if(hours[i] >= target)
+            if(hour >= target)
             count ++;
         }
         return count;
